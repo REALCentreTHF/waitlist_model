5e89fdd465b299eae916acb2d68223b46f712e2c
Zeyad Issa <zeyadissa@Zeyads-Macbook-Pro.local>
2025-01-22 14:20:00 +0000

    init

--- src/ge_briefing/eda.R
+++ src/ge_briefing/eda.R
@@ -1,4 +1,4 @@
-# Dependancies -----------
+# DEPENDANCIES -----------
 
 source('const/glob.R')
 source('src/functions.R')
@@ -8,11 +8,16 @@ df_2 <- data.table::fread('const/df_2.csv')
 df_a <- data.table::fread('const/df_a.csv')
 df_c <- data.table::fread('const/df_c.csv')
 
-# Raw inputs ----------------------------------------------------------
+# RAW INPUTS ----------------------------------------------------------
 
 sim_time <- (11*12)
+
+#Long-term year average
 capacity_10yr <- 1.038^(1/12)
-capacity_5yr <- 1.06^(1/12)
+#No change 
+capacity_flat <- 1^(1/12)
+#CAP growth from -12t to -35t
+capacity_base <- 1.015^(1/12)
   
 # Fixed capacity by year
 base_capacity <- df_1 %>%
@@ -61,82 +66,17 @@ capacity_ideal_10yr <- CreateCapacity(x=sim_time,
                             growth=capacity_10yr,
                             base_capacity=base_capacity)
 
-capacity_0_5 <- CreateCapacity(x=60,
-                           specialties=specs,
-                           growth=capacity_5yr,
-                           base_capacity=base_capacity)
+capacity_ideal_flat <- CreateCapacity(x=sim_time,
+                                      specialties=specs,
+                                      growth=capacity_flat,
+                                      base_capacity=base_capacity)
 
-midterm_capacity <- (capacity_0_5%>%filter(t==max(t)) %>% select(!t))$cap
-midterm_referral <- starting_average*(referral_growth)^(60)
-capacity_ratio <- midterm_referral/midterm_capacity
+capacity_ideal_base <- CreateCapacity(x=sim_time,
+                                      specialties=specs,
+                                      growth=capacity_base,
+                                      base_capacity=base_capacity)
 
-capacity_5_10 <- CreateReferrals(min_x=1,
-                                 max_x=sim_time,
-                                 specialty = specs,
-                                 growth = referral_growth,
-                                 starting_value = midterm_referral,
-                                 jitter_factor = 0) %>%
-  filter(t <= sim_time - 60) %>%
-  select(!i) %>%
-  mutate(t= 60 + t) %>%
-  rename('cap'=open) %>%
-  #decline by x val to meet drop-off rates
-  mutate(cap = cap*0.9758611)
-
-capacity_ideal_5yr <- rbind(capacity_0_5,capacity_5_10)
-
-# Waitlist Size ----------------------------------------------------------
-
-full_10yr_data <- CreateData(df_data = df_2,
-                       ref_growth = referral_growth,
-                       capacity = capacity_ideal_10yr,
-                       policy = 0.5,
-                       breach_limit = 4,
-                       jitter_factor = 0,
-                       a_lim = 0.75)$full_data %>%
-  mutate(wait_time = z * i) %>%
-  group_by(t) %>%
-  summarise(wait_times = sum(wait_time,na.rm=T),
-            z = sum(z,na.rm=T)) %>%
-  mutate(mean = (wait_times/z)*4.333)
-
-
-full_5yr_data <- CreateData(df_data = df_2,
-                              ref_growth = referral_growth,
-                              capacity = capacity_ideal_5yr,
-                              policy = 0.5,
-                              breach_limit = 4,
-                              jitter_factor = 0,
-                              a_lim = 0.75)$full_data %>%
-  mutate(wait_time = z * i) %>%
-  group_by(t) %>%
-  summarise(wait_times = sum(wait_time,na.rm=T),
-            z = sum(z,na.rm=T)) %>%
-  mutate(mean = (wait_times/z)*4.333)
-
-# Waitlist Activity & Costs -------
-
-breach_10yr_data <- CreateData(df_data = df_2,
-                    ref_growth = referral_growth,
-                    capacity = capacity_ideal_10yr,
-                    policy = 0.5,
-                    breach_limit = 4,
-                    jitter_factor = 0,
-                    a_lim = 0.75)$breaches %>%
-  mutate(ratio = breach/tot) %>%
-  add_row(old_data %>% filter(t == 0))
-
-breach_5yr_data <- CreateData(df_data = df_2,
-                               ref_growth = referral_growth,
-                               capacity = capacity_ideal_5yr,
-                               policy = 0.5,
-                               breach_limit = 4,
-                               jitter_factor = 0,
-                               a_lim = 0.75)$breaches %>%
-  mutate(ratio = breach/tot) %>%
-  add_row(old_data %>% filter(t == 0))
-
-# Costs ------
+# ACTIVITY ------
 
 activity_10yr_ideal <- CreateData(df_data = df_2,
                              ref_growth = referral_growth,
@@ -155,23 +95,12 @@ activity_10yr_ideal <- CreateData(df_data = df_2,
     dc = costs$admit_ratio * costs$daycase_ratio * cap,
     ord = costs$admit_ratio * costs$ordinary_ratio * cap) %>%
   select(!cap) %>%
-  mutate(date = max(lubridate::as_date(df_1$date)) + months(t))
+  mutate(date = max(lubridate::as_date(df_1$date)) + months(t)) |> 
+  dplyr::select(!c(t,s))
 
-cost_10yr_ideal <- activity_10yr_ideal %>%
-  mutate(ncl_costing = ncl * ncl_cost,
-         cl_costing = cl * cl_cost,
-         in_cost = ord * in_cost,
-         dc_cost = dc * dc_cost,
-         proc_cost = proc * proc_cost) %>%
-  select(date,ncl_costing,cl_costing,in_cost,dc_cost,proc_cost) %>%
-  pivot_longer(cols=!date,names_to='type',values_to='cost') %>%
-  mutate(date = zoo::as.yearmon(date)) %>%
-  group_by(year(date),type)%>%
-  summarise(cost=sum(cost,na.rm=T)/1e9,)
-
-activity_5yr_ideal <- CreateData(df_data = df_2,
+activity_flat_ideal <- CreateData(df_data = df_2,
                                   ref_growth = referral_growth,
-                                  capacity = capacity_ideal_5yr,
+                                  capacity = capacity_ideal_flat,
                                   policy = 0.5,
                                   breach_limit = 4,
                                   jitter_factor = 0,
@@ -186,68 +115,16 @@ activity_5yr_ideal <- CreateData(df_data = df_2,
     dc = costs$admit_ratio * costs$daycase_ratio * cap,
     ord = costs$admit_ratio * costs$ordinary_ratio * cap) %>%
   select(!cap) %>%
-  mutate(date = max(lubridate::as_date(df_1$date)) + months(t))
+  mutate(date = max(lubridate::as_date(df_1$date)) + months(t)) |> 
+  dplyr::select(!c(t,s))
 
-cost_5yr_ideal <- activity_5yr_ideal %>%
-  mutate(ncl_costing = ncl * ncl_cost,
-         cl_costing = cl * cl_cost,
-         in_cost = ord * in_cost,
-         dc_cost = dc * dc_cost,
-         proc_cost = proc * proc_cost) %>%
-  select(date,ncl_costing,cl_costing,in_cost,dc_cost,proc_cost) %>%
-  pivot_longer(cols=!date,names_to='type',values_to='cost') %>%
-  mutate(date = zoo::as.yearmon(date)) %>%
-  group_by(year(date),type)%>%
-  summarise(cost=sum(cost,na.rm=T)/1e9,)
-
-growth_assumptions <- growth_assumptions %>%
-  group_by(scenario) %>%
-  mutate(
-    prod = case_when(
-      is.na(val_prod/lag(val_prod)) == T ~ 1,
-      T ~val_prod/lag(val_prod)),
-    pay = case_when(
-      is.na(val_pay/lag(val_pay)) == T ~ 1,
-      T ~ val_pay/lag(val_pay))
-  ) %>%
-  select(!c(val_prod,val_pay))
-
-final_cost_data <- cost_10yr_ideal %>%
-  rename(year='year(date)',
-         cost_10yr = 'cost') %>%
-  left_join(.,cost_5yr_ideal %>%
-              rename(year='year(date)',
-                     cost_5yr = 'cost'),
-            by=c('year','type')) %>%
-  filter(year != 2036) %>%
-  group_by(type) %>%
-  mutate(drug = 1.0489,
-         val_drug = cumprod(drug)) %>%
-  left_join(.,
-            growth_assumptions %>%
-              filter(scenario == '10-year') %>%
-              ungroup()%>%
-              select(fyear,prod,pay),
-            by=c('year'='fyear')) %>%
-  ungroup()%>%
-  rowwise() %>%
-  mutate(index = CreateIndex(w=w,
-                             d = d,
-                             r = r,
-                             prod = prod,
-                             drug = val_drug,
-                             pay = pay,
-                             deflator = 1),
-         cost_5yr = cost_5yr * index,
-         cost_10yr = cost_10yr * index) %>%
-  group_by(year) %>%
-  summarise(cost_10yr = sum(cost_10yr,na.rm=T),
-            cost_5yr = sum(cost_5yr,na.rm=T))
-
-data_2023 <-
-  df_2 %>%
-  filter(t >= max(t)-12) %>%
-  summarise(cap = sum(completed,na.rm=T)) %>%
+activity_base_ideal <- CreateData(df_data = df_2,
+                                  ref_growth = referral_growth,
+                                  capacity = capacity_ideal_base,
+                                  policy = 0.5,
+                                  breach_limit = 4,
+                                  jitter_factor = 0,
+                                  a_lim = 0.75)$capacity %>%
   mutate(
     first = cap,
     fups = costs$fup_ratio * cap,
@@ -258,13 +135,59 @@ data_2023 <-
     dc = costs$admit_ratio * costs$daycase_ratio * cap,
     ord = costs$admit_ratio * costs$ordinary_ratio * cap) %>%
   select(!cap) %>%
+  mutate(date = max(lubridate::as_date(df_1$date)) + months(t)) |> 
+  dplyr::select(!c(t,s))
+
+# COSTS ------
+
+cost_10yr_ideal <- activity_10yr_ideal %>%
   mutate(ncl_costing = ncl * ncl_cost,
          cl_costing = cl * cl_cost,
          in_cost = ord * in_cost,
          dc_cost = dc * dc_cost,
-         proc_cost = proc * proc_cost)
+         proc_cost = proc * proc_cost) %>%
+  select(date,ncl_costing,cl_costing,in_cost,dc_cost,proc_cost) %>%
+  pivot_longer(cols=!c(date),names_to='metric',values_to='values') |> 
+  dplyr::group_by(date) |> 
+  dplyr::summarise(ideal_cost = sum(values,na.rm=T))
 
-write.csv(cost_5yr_ideal,'output/hiba_output/costs_5yr.csv')
-write.csv(cost_10yr_ideal,'output/hiba_output/costs_10yr_ideal.csv')
-write.csv(activity_5yr_ideal,'output/hiba_output/activity_5yr_ideal.csv')
-write.csv(activity_10yr_ideal,'output/hiba_output/activity_10yr_ideal.csv')
+cost_base_ideal <- activity_base_ideal %>%
+  mutate(ncl_costing = ncl * ncl_cost,
+         cl_costing = cl * cl_cost,
+         in_cost = ord * in_cost,
+         dc_cost = dc * dc_cost,
+         proc_cost = proc * proc_cost) %>%
+  select(date,ncl_costing,cl_costing,in_cost,dc_cost,proc_cost) %>%
+  pivot_longer(cols=!c(date),names_to='metric',values_to='values') |> 
+  dplyr::group_by(date) |> 
+  dplyr::summarise(base_cost = sum(values,na.rm=T))
+
+cost_flat_ideal <- activity_flat_ideal %>%
+  mutate(ncl_costing = ncl * ncl_cost,
+         cl_costing = cl * cl_cost,
+         in_cost = ord * in_cost,
+         dc_cost = dc * dc_cost,
+         proc_cost = proc * proc_cost) %>%
+  select(date,ncl_costing,cl_costing,in_cost,dc_cost,proc_cost) %>%
+  pivot_longer(cols=!c(date),names_to='metric',values_to='values') |> 
+  dplyr::group_by(date) |> 
+  dplyr::summarise(flat_cost = sum(values,na.rm=T))
+
+# FINAL DATASET ------
+
+final_dataset <- activity_10yr_ideal |> 
+  dplyr::select(date,first) |> 
+  dplyr::rename(ideal_pathways = 'first') |> 
+  dplyr::left_join(
+    activity_base_ideal |> 
+      dplyr::select(date,first) |> 
+      dplyr::rename(base_pathways = 'first')) |> 
+  dplyr::left_join(
+    activity_flat_ideal |> 
+      dplyr::select(date,first) |> 
+      dplyr::rename(flat_pathways = 'first')) |> 
+  dplyr::left_join(cost_10yr_ideal) |> 
+  dplyr::left_join(cost_base_ideal) |> 
+  dplyr::left_join(cost_flat_ideal)
+
+write.csv(final_dataset,'output/hiba_output/final_dataset.csv')
